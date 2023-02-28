44c28bdb0ef403b59c80b0aa5164b821defcb0a8
Moamen-Wady <moamen.wady@gmail.com>
2023-02-28 03:22:04 +0200

    daman

---
+++ Back/app.js
@@ -0,0 +1,24 @@
+const express = require( 'express' );
+const cors = require( 'cors' );
+const http = require( 'http' );
+const helmet = require( 'helmet' );
+const compression = require( 'compression' );
+const app = express();
+const corsOptions = {
+    origin: ['http://localhost:3000', 'http://localhost:3005'],
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use( cors( corsOptions ) );
+app.use( express.json() );
+app.use( helmet() );
+app.use( compression() );
+app.use( express.urlencoded( { extended: true } ) );
+
+const serverx = http.createServer( app );
+serverx.listen( port, console.log( "ok" ) );
+
+
+app.get( "/", ( req, res ) => {
+    res.send( 'ok' )
+} )
